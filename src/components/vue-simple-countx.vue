b6eae6029d2e23c0d083e720479703224556a6cb
hareland <--get>
2023-12-05 04:58:51 +0100

    refactor: cleaning up

--- src/components/vue-simple-countx.vue
+++ src/components/vue-simple-countx.vue
@@ -2,7 +2,7 @@
   {{ currentValue }}
 </template>
 <script lang="ts" setup>
-import {computed, onBeforeUnmount, onMounted, onUnmounted, ref, useSlots} from "vue";
+import {computed, onBeforeMount, onBeforeUnmount, ref} from "vue";
 
 const props = withDefaults(defineProps<{
   timeout?: number;
@@ -13,11 +13,14 @@ const props = withDefaults(defineProps<{
   steps: 1,
 });
 
-const iterations = ref(0);
+
+let iterations = 0;
 let interval: NodeJS.Timeout;
 
-const targetValue = computed(() => Number(props.value))
+const initialValue = ref(props.value);
 const currentValue = ref(0);
+const targetValue = computed(() => Number(props.value));
+
 const getNextValue = () => {
   if (targetValue.value > 0) {
     const nextValue = currentValue.value + props.steps;
@@ -33,16 +36,16 @@ const getNextValue = () => {
     return nextValue;
   }
 
-  return nextValue + (props.steps + 1)
+  return nextValue + (props.steps + 1);
 };
 
 const intervalHandler = () => {
   if (interval && currentValue.value === targetValue.value) {
-    clearInterval(interval)
+    unmount();
     return;
   }
 
-  iterations.value++;
+  iterations++;
 
   currentValue.value = getNextValue();
 }
@@ -51,23 +54,20 @@ const mount = () => {
   interval = setInterval(intervalHandler, props.timeout)
 }
 
-const reset = () => {
-  if (interval) {
-    clearInterval(interval)
-  }
-
-  iterations.value = 0;
-  mount();
-}
-
 const unmount = () => {
   if (interval) {
     clearInterval(interval);
   }
 }
 
+const reset = () => {
+  unmount();
+  iterations = 0;
+  currentValue.value = initialValue.value;
+  mount();
+}
 
-onMounted(mount);
+onBeforeMount(mount);
 onBeforeUnmount(unmount);
 
 defineExpose({
